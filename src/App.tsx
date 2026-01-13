63ca42f7decaf9ccb9073963c46d5657e66d5b28
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-01-13 13:21:29 +0530

    Api is configured and Pages setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-
-import Test from "./components/test";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Gigs from "./pages/Gigs";
+import GigDetails from "./pages/GigDetails";
+import PostGig from "./pages/PostGig";
 
 function App() {
   return (
-    <>
-      <Test />
-    </>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/gigs" element={<Gigs />} />
+      <Route path="/gigs/:id" element={<GigDetails />} />
+      <Route path="/post-gig" element={<PostGig />} />
+    </Routes>
   );
 }
 
