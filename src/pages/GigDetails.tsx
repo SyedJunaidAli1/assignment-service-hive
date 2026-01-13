00b604a950e081d75c87c9dd9b4179b6a6a3eae9
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-01-13 13:34:50 +0530

    default export for all the pages

---
+++ src/pages/GigDetails.tsx
@@ -0,0 +1,9 @@
+const GigDetails = () => {
+  return (
+    <div>
+      <h1>Gig Details</h1>
+    </div>
+  );
+};
+
+export default GigDetails;
---
+++ src/pages/PostGig.tsx
@@ -0,0 +1,9 @@
+const PostGig = () => {
+  return (
+    <div>
+      <h1>Post Gig</h1>
+    </div>
+  );
+};
+
+export default PostGig;
